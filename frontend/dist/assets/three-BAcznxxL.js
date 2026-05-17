0a9f7f0b824d49491a8458b89a355faf5d3dcd8b
Md Danish Iftekhar <danish90654@gmail.com>
2026-05-17 21:56:25 +0530

    Fix Netlify build and routing

--- frontend/dist/assets/three-BAcznxxL.js
+++
@@ -1,1 +0,0 @@
-import{e as o,g as a}from"./vendor-CkjagKCp.js";var e={},r;function i(){if(r)return e;r=1;var t=o();return e.createRoot=t.createRoot,e.hydrateRoot=t.hydrateRoot,e}var n=i();const R=a(n);export{R};
